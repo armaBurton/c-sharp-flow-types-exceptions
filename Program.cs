46cb119fc318397007a71ea5c055c2e14fd7c0bd
Arma Burton <arma@armaburton.com>
2023-05-10 14:03:01 -0700

    logical operators

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int a = 3;
+// int b = a++;
+// WriteLine($"a is {a}, b is {b}");
+// WriteLine("\n");
+
+// int c = 3, d = ++c;
+// WriteLine($"c is {c}, d is {d}");
+// WriteLine("\n");
+
+// int e = 11, f = 3;
+// WriteLine($"e is {e}, f is {f}");
+// WriteLine($"e + f = {e + f}");
+// WriteLine($"e - f = {e - f}");
+// WriteLine($"e * f = {e * f}");
+// WriteLine($"e / f = {e / f}");
+// WriteLine($"e % f = {e % f}");
+// WriteLine("\n");
+
+// double g = 11.0;
+// WriteLine($"g is {g:N1}, f is {f}");
+// WriteLine ($"g / f = {g / f}");
+// WriteLine("\n");
+
+bool a = true, b = false;
+WriteLine($"AND   | a      | b     ");
+WriteLine($"a     | { a & a, -5}  | {a & b, -5}");
+WriteLine($"a     | { b & a, -5}  | {b & b, -5}");
+WriteLine();
