c627da728641f4072a3ffa35838fbf8fce111fee
Arma Burton <arma@armaburton.com>
2023-05-10 20:36:14 -0700

    make it look nice

--- Program.cs
+++ Program.cs
@@ -21,28 +21,44 @@
 // WriteLine ($"g / f = {g / f}");
 // WriteLine("\n");
 
-bool a = true, b = false;
-WriteLine($"AND   | a      | b     ");
-WriteLine($"a     | { a & a, -5}  | {a & b, -5}");
-WriteLine($"a     | { b & a, -5}  | {b & b, -5}");
-WriteLine();
-WriteLine($"OR    | a      | b     ");
-WriteLine($"a     | { a & a, -5}  | {a & b, -5}");
-WriteLine($"a     | { b & a, -5}  | {b & b, -5}");
-WriteLine();
-WriteLine($"XOR   | a      | b     ");
-WriteLine($"a     | { a ^ a, -5}  | {a ^ b, -5}");
-WriteLine($"a     | { b ^ a, -5}  | {b ^ b, -5}");
-WriteLine();
+// bool a = true, b = false;
+// WriteLine($"AND   | a      | b     ");
+// WriteLine($"a     | { a & a, -5}  | {a & b, -5}");
+// WriteLine($"a     | { b & a, -5}  | {b & b, -5}");
+// WriteLine();
+// WriteLine($"OR    | a      | b     ");
+// WriteLine($"a     | { a & a, -5}  | {a & b, -5}");
+// WriteLine($"a     | { b & a, -5}  | {b & b, -5}");
+// WriteLine();
+// WriteLine($"XOR   | a      | b     ");
+// WriteLine($"a     | { a ^ a, -5}  | {a ^ b, -5}");
+// WriteLine($"a     | { b ^ a, -5}  | {b ^ b, -5}");
+// WriteLine();
 
-static bool DoStuff(){
-  WriteLine("I am doing stuff.");
-  return true;
+// static bool DoStuff(){
+//   WriteLine("I am doing stuff.");
+//   return true;
+// }
+
+// WriteLine();
+// WriteLine($"a & DoStuff() = {a & DoStuff()}");
+// WriteLine($"b & DoStuff() = {b & DoStuff()}");
+// WriteLine();
+// WriteLine($"a && DoStuff() = {a && DoStuff()}");
+// WriteLine($"b && DoStuff() = {b && DoStuff()}");
+
+static string ToBinaryString(int value){
+  return Convert.ToString(value, toBase:2).PadLeft(8, '0');
 }
 
-WriteLine();
-WriteLine($"a & DoStuff() = {a & DoStuff()}");
-WriteLine($"b & DoStuff() = {b & DoStuff()}");
-WriteLine();
-WriteLine($"a && DoStuff() = {a && DoStuff()}");
-WriteLine($"b && DoStuff() = {b && DoStuff()}");
+int a = 10, b = 6;
+
+WriteLine($"     a = {a}, {ToBinaryString(a)}");
+WriteLine($"     b =  {b}, {ToBinaryString(b)}");
+WriteLine($" a & b =  {a & b}, {ToBinaryString(a & b)}");
+WriteLine($" a | b = {a | b}, {ToBinaryString(a | b)}");
+WriteLine($" a ^ b = {a ^ b}, {ToBinaryString(a ^ b)}");
+WriteLine($"a << 3 = {a << 3}, {ToBinaryString(a << 3)}");
+WriteLine($"b >> 1 =  {b >> 1}, {ToBinaryString(b >> 1)}");
+
+
