ebb7f0649eab7246c649108defbf2fa38137bb21
Arma Burton <arma@armaburton.com>
2023-05-10 21:24:26 -0700

    oop

--- Program.cs
+++ Program.cs
@@ -1,4 +1,48 @@
-﻿
+﻿Animal?[] animals = new Animal?[]{
+  new Cat {
+    Name = "Karen", 
+    Born = new(year: 2022, month: 8, day: 23), 
+    Legs = 4, 
+    isDomestic = true
+  }, 
+  null, 
+  new Cat {
+    Name = "Mufasa",
+    Born = new(year: 1994, month: 6, day: 12)
+  },
+  new Spider {
+    Name = "Sid Vicious",
+    Born = DateTime.Today,
+    isPoisonous = true
+  },
+  new Spider {
+    Name = "Captain Furry",
+    Born = DateTime.Today
+  }
+};
+
+foreach (Animal? animal in animals){
+  string message;
+
+  switch (animal){
+    case Cat fourLeggedCat when fourLeggedCat.Legs == 4:
+      message = $"The cat name {fourLeggedCat.Name} has four legs.";
+      break;
+    case Cat wildCat when wildCat.isDomestic == false:
+      message = $"The non-domestic cat is named {wildCat.name}";
+      break;
+    case Cat cat:
+      message = $"The cat is named {cat.Name}.";
+      break;
+    case Spider spider when spider.isPoisonous:
+      message = $"The {spider.Name} spider is poisonous. Run!";
+      break;
+    case null:
+      message = "The animal is null";
+      break;
+  }
+  WriteLine($"switch statement: {message}");
+}
 
 
 
