7dd78cf270662d2dd6cb1f8a2a48457d3c9c0e39
Arma Burton <arma@armaburton.com>
2023-05-11 00:06:36 -0700

    array

--- Animals.cs
+++ Animals.cs
@@ -1,13 +1,13 @@
-class Animal{
-  public string? Name;
-  public DateTime Born;
-  public byte Legs;
-}
+// class Animal{
+//   public string? Name;
+//   public DateTime Born;
+//   public byte Legs;
+// }
 
-class Cat : Animal{
-  public bool isDomestic;
-}
+// class Cat : Animal{
+//   public bool isDomestic;
+// }
 
-class Spider: Animal{
-  public bool isPoisonous;
-}
+// class Spider: Animal{
+//   public bool isPoisonous;
+// }
--- Program.cs
+++ Program.cs
@@ -1,60 +1,67 @@
-﻿Animal?[] animals = new Animal?[]{
-  new Cat {
-    Name = "Karen", 
-    Born = new(year: 2022, month: 8, day: 23), 
-    Legs = 4, 
-    isDomestic = true
-  }, 
-  null, 
-  new Cat {
-    Name = "Mufasa",
-    Born = new(year: 1994, month: 6, day: 12)
-  },
-  new Spider {
-    Name = "Sid Vicious",
-    Born = DateTime.Today,
-    isPoisonous = true
-  },
-  new Spider {
-    Name = "Captain Furry",
-    Born = DateTime.Today
-  }
-};
-
-foreach (Animal? animal in animals){
-  string message = animal switch{
-    Cat fourLeggedCat when fourLeggedCat.Legs == 4 => $"The cat {fourLeggedCat.Name} has four legs.",
-    Cat wildCat when wildCat.isDomestic == false => $"The non-domestic cat is named {wildCat.Name}.",
-    Cat cat => $"The cat is named {cat.Name}.",
-    Spider spider when spider.isPoisonous => $"The {spider.Name} spider is poisonous. Run!",
-    null => "The animal is Null",
-    _ => $"The animal named {animal.Name} is a {animal.GetType().Name}."
-  };
-  WriteLine($"switch statement: {message}");
-
-  switch (animal){
-    case Cat fourLeggedCat when fourLeggedCat.Legs == 4:
-      message = $"The cat name {fourLeggedCat.Name} has four legs.";
-      break;
-    case Cat wildCat when wildCat.isDomestic == false:
-      message = $"The non-domestic cat is named {wildCat.Name}";
-      break;
-    case Cat cat:
-      message = $"The cat is named {cat.Name}.";
-      break;
-    default:
-      message = $"The animal named {animal.Name} is a {animal.GetType().Name}.";
-      break;
-    case Spider spider when spider.isPoisonous:
-      message = $"The {spider.Name} spider is poisonous. Run!";
-      break;
-    case null:
-      message = "The animal is null";
-      break;
-  }
-
-  WriteLine($"switch expression: {message}");
+﻿string[] names2 = new[] {"Kate", "Jack", "Rebecca", "Tom"};
+foreach(string name in names2){
+  WriteLine(name);
 }
+
+
+
+// Animal?[] animals = new Animal?[]{
+//   new Cat {
+//     Name = "Karen", 
+//     Born = new(year: 2022, month: 8, day: 23), 
+//     Legs = 4, 
+//     isDomestic = true
+//   }, 
+//   null, 
+//   new Cat {
+//     Name = "Mufasa",
+//     Born = new(year: 1994, month: 6, day: 12)
+//   },
+//   new Spider {
+//     Name = "Sid Vicious",
+//     Born = DateTime.Today,
+//     isPoisonous = true
+//   },
+//   new Spider {
+//     Name = "Captain Furry",
+//     Born = DateTime.Today
+//   }
+// };
+
+// foreach (Animal? animal in animals){
+//   string message = animal switch{
+//     Cat fourLeggedCat when fourLeggedCat.Legs == 4 => $"The cat {fourLeggedCat.Name} has four legs.",
+//     Cat wildCat when wildCat.isDomestic == false => $"The non-domestic cat is named {wildCat.Name}.",
+//     Cat cat => $"The cat is named {cat.Name}.",
+//     Spider spider when spider.isPoisonous => $"The {spider.Name} spider is poisonous. Run!",
+//     null => "The animal is Null",
+//     _ => $"The animal named {animal.Name} is a {animal.GetType().Name}."
+//   };
+//   WriteLine($"switch statement: {message}");
+
+//   switch (animal){
+//     case Cat fourLeggedCat when fourLeggedCat.Legs == 4:
+//       message = $"The cat name {fourLeggedCat.Name} has four legs.";
+//       break;
+//     case Cat wildCat when wildCat.isDomestic == false:
+//       message = $"The non-domestic cat is named {wildCat.Name}";
+//       break;
+//     case Cat cat:
+//       message = $"The cat is named {cat.Name}.";
+//       break;
+//     default:
+//       message = $"The animal named {animal.Name} is a {animal.GetType().Name}.";
+//       break;
+//     case Spider spider when spider.isPoisonous:
+//       message = $"The {spider.Name} spider is poisonous. Run!";
+//       break;
+//     case null:
+//       message = "The animal is null";
+//       break;
+//   }
+
+//   WriteLine($"switch expression: {message}");
+// }
 
 
 
