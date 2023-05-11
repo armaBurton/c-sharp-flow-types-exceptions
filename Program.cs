e86484ad76eb31cd98b6df2e300bee1f55c07aa2
Arma Burton <arma@armaburton.com>
2023-05-10 20:21:30 -0700

    logical operators

--- Program.cs
+++ Program.cs
@@ -34,3 +34,15 @@ WriteLine($"XOR   | a      | b     ");
 WriteLine($"a     | { a ^ a, -5}  | {a ^ b, -5}");
 WriteLine($"a     | { b ^ a, -5}  | {b ^ b, -5}");
 WriteLine();
+
+static bool DoStuff(){
+  WriteLine("I am doing stuff.");
+  return true;
+}
+
+WriteLine();
+WriteLine($"a & DoStuff() = {a & DoStuff()}");
+WriteLine($"b & DoStuff() = {b & DoStuff()}");
+WriteLine();
+WriteLine($"a && DoStuff() = {a && DoStuff()}");
+WriteLine($"b && DoStuff() = {b && DoStuff()}");
